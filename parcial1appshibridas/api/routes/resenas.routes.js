b2fb7af0d6d6788fb7125e80c2b651412933e1ac
nicolaslizaso-dv <nicolas.lizaso@davinci.edu.ar>
2026-09-25 09:43:18 -0300

    Delete parcial1appshibridas directory

--- parcial1appshibridas/api/routes/resenas.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import * as resenasController from "../controllers/resenas.controller.js"
-
-const router = Router()
-
-router.get("/api/resenas", resenasController.listar)
-router.post("/api/resenas", resenasController.crear)
-
-export default router
